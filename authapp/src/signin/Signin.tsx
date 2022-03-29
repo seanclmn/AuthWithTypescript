42370be6b74bcbaf4ba4396ba4651a7af4ec300a
secolema <seanclmn1@gmail.com>
2022-03-28 23:28:13 -0400

    tailwind

--- authapp/src/signin/Signin.tsx
+++ authapp/src/signin/Signin.tsx
@@ -1,13 +1,23 @@
+// @ts-nocheck
 
 import React from 'react'
+import {useForm} from 'react-hook-form'
 
-interface Props {}
 
-function Signin(props: Props) {
-    const {} = props
+function Signin() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = (data,event) => {
+        event.preventDefault()
+        console.log(data);}
 
     return (
-        <p>signin</p>
+        <form className='flex flex-col items-center' onSubmit={handleSubmit(onSubmit)}>
+            <p>signin</p>
+            <input {...register("email", { required: true })} className="border"/>
+            <input {...register("password", { required: true })} className="border"/>
+            <button>submit</button>
+        </form>
+
     )
 }
 
